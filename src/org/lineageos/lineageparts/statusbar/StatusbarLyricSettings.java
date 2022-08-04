886488b43b47cd3e9ad222b1c4e1c22a0692493d
Kujou Yuko <i@779.moe>
2022-08-04 01:35:52 +0000

    LineageParts: Add status bar lyric [2/2]
    Signed-off-by: Kujou Yuko <i@779.moe>
    Change-Id: I944afb9e961eee72ce7fd183f2a97a9210f6c35a
    Signed-off-by: Wrdn28 <ifloraxd@gmail.com>

---
+++ src/org/lineageos/lineageparts/statusbar/StatusbarLyricSettings.java
@@ -0,0 +1,39 @@
+/*
+ * SPDX-FileCopyrightText: OrionOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package org.lineageos.lineageparts.statusbar;
+
+import android.os.Bundle;
+import android.content.Context;
+import android.util.ArraySet;
+
+import androidx.preference.Preference;
+
+import org.lineageos.lineageparts.R;
+import org.lineageos.lineageparts.SettingsPreferenceFragment;
+import org.lineageos.lineageparts.search.BaseSearchIndexProvider;
+import org.lineageos.lineageparts.search.Searchable;
+
+import java.util.Set;
+
+public class StatusbarLyricSettings extends SettingsPreferenceFragment
+        implements Searchable {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.status_bar_lyric_settings);
+    }
+
+    public static final Searchable.SearchIndexProvider SEARCH_INDEX_DATA_PROVIDER =
+            new BaseSearchIndexProvider() {
+
+        @Override
+        public Set<String> getNonIndexableKeys(Context context) {
+            final Set<String> result = new ArraySet<String>();
+            return result;
+        }
+    };
+}
