9e3266372b2cd3c212828194369c693a6c011db6
Nipin NA <nipinna0@gmail.com>
2025-10-03 20:54:00 +0200

    StatusBarSettings: Hide battery percentage catagory when ios icons are enabled
    [nurkeinneid: Slightly improved through better naming]
    Signed-off-by: NurKeinNeid <nurkeinneid@derpfest.org>

--- src/org/lineageos/lineageparts/statusbar/StatusBarSettings.java
+++ src/org/lineageos/lineageparts/statusbar/StatusBarSettings.java
@@ -46,6 +46,8 @@ public class StatusBarSettings extends SettingsPreferenceFragment {
     private static final String STATUS_BAR_QUICK_QS_PULLDOWN = "qs_quick_pulldown";
 
     private static final int STATUS_BAR_BATTERY_STYLE_TEXT = 2;
+    private static final int STATUS_BAR_BATTERY_STYLE_IOS_15 = 5;
+    private static final int STATUS_BAR_BATTERY_STYLE_IOS_16 = 6;
 
     private static final int QS_BRIGHTNESS_SLIDER_HIDDEN = 0;
 
@@ -183,7 +185,11 @@ public class StatusBarSettings extends SettingsPreferenceFragment {
     }
 
     private void enableStatusBarBatteryDependents(int batteryIconStyle) {
-        mStatusBarBatteryShowPercent.setEnabled(batteryIconStyle != STATUS_BAR_BATTERY_STYLE_TEXT);
+        boolean shouldDisablePercentage = batteryIconStyle == STATUS_BAR_BATTERY_STYLE_TEXT
+                || batteryIconStyle == STATUS_BAR_BATTERY_STYLE_IOS_15
+                || batteryIconStyle == STATUS_BAR_BATTERY_STYLE_IOS_16;
+        
+        mStatusBarBatteryShowPercent.setEnabled(!shouldDisablePercentage);
     }
 
     private int getNetworkTrafficStatus() {
